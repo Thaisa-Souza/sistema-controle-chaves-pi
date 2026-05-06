0a0ce7425e042411228142969157080ce54d9fe2
Thaisa-Souza <thaisadesouza.imoveis@gmail.com>
2026-05-06 12:23:15 -0300

    Adiciona edição e exclusão de imóveis

--- imoveis/views.py
+++ imoveis/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Imovel
 from .forms import ImovelForm
+from django.shortcuts import get_object_or_404
 
 
 def imovel_list(request):
@@ -20,4 +21,34 @@ def imovel_create(request):
 
     return render(request, 'imoveis/form.html', {
         'form': form
+    })
+
+
+def imovel_update(request, pk):
+    imovel = get_object_or_404(Imovel, pk=pk)
+
+    form = ImovelForm(
+        request.POST or None,
+        request.FILES or None,
+        instance=imovel
+    )
+
+    if form.is_valid():
+        form.save()
+        return redirect('imovel_list')
+
+    return render(request, 'imoveis/form.html', {
+        'form': form
+    })
+
+
+def imovel_delete(request, pk):
+    imovel = get_object_or_404(Imovel, pk=pk)
+
+    if request.method == 'POST':
+        imovel.delete()
+        return redirect('imovel_list')
+
+    return render(request, 'imoveis/confirm_delete.html', {
+        'imovel': imovel
     })
