e318570a07b8c8c2dfa284a3615d2f3c99e3908b
Thaisa-Souza <thaisadesouza.imoveis@gmail.com>
2026-05-06 12:16:45 -0300

    Implementa cadastro e listagem de imóveis

--- imoveis/urls.py
+++ imoveis/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('', views.imovel_list, name='imovel_list'),
+    path('novo/', views.imovel_create, name='imovel_create'),
 ]
-
--- imoveis/views.py
+++ imoveis/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Imovel
+from .forms import ImovelForm
 
-# Create your views here.
+
+def imovel_list(request):
+    imoveis = Imovel.objects.all().order_by('codigo')
+
+    return render(request, 'imoveis/list.html', {
+        'imoveis': imoveis
+    })
+
+
+def imovel_create(request):
+    form = ImovelForm(request.POST or None, request.FILES or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('imovel_list')
+
+    return render(request, 'imoveis/form.html', {
+        'form': form
+    })
