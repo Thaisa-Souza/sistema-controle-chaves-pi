9d04939ac0a24ee7ea9da671c97a1ae01568ff5a
Thaisa-Souza <thaisadesouza.imoveis@gmail.com>
2026-05-08 03:35:16 -0300

    Configura projeto para deploy no PythonAnywhere

--- controle_chaves/settings.py
+++ controle_chaves/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-r7^vww9mmc^+ly^n-klpq!82=9pk$fs5_&utq(vrggjrbhb7k)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['controledechaves.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -116,7 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 import os
